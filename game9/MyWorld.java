eaf56e23a74b3be3b7a39caa6588e193a66f0072
17119Tobu <0987654321qazedc@gmail.com>
2022-01-27 16:57:24 +0900

    20220127更新

--- game9/MyWorld.java
+++ game9/MyWorld.java
@@ -8,17 +8,22 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
-
+    private int time = 0;
     /**
      * Constructor for objects of class MyWorld.
      * 
      */
+    
     public MyWorld()
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(1000, 650, 1); 
-        addObject( new egg(), 100, 600 );
         
+        super(1000, 650, 1);
+        showText( "1ぷんかん　にげて！^^", 250, 100 );
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+         Greenfoot.setSpeed(50);
+         prepare();
+        addObject( new egg(), 100, 600 );
+        addObject( new otama(), 600, 400 );
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels. 
         
         int blocksize = 50;
@@ -29,4 +34,21 @@ public class MyWorld extends World
         addObject( new Load(), 350, y-blocksize);            
         
     }
+    private void prepare()
+    {
+    time = 3300;
+    //adding objects here
+    }
+    public void act()
+{
+    time--;
+    if (time == 0)
+    {
+        removeObjects(getObjects(null));
+        prepare();
+        /** OR **/
+        showText( "にげて　どうするの？", 150, 100 );
+    }
+}
+ 
 }
